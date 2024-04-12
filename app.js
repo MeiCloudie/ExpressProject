4261a1dd8674a7b20e5b6bbbccf6a7b5be927252
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-04-12 10:32:57 +0700

    fix: add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path")
 var cookieParser = require("cookie-parser")
 var logger = require("morgan")
 var mongoose = require("mongoose")
+var cors = require("cors")
 
 var app = express()
 mongoose
@@ -18,6 +19,7 @@ mongoose
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
+app.use(cors())
 app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
